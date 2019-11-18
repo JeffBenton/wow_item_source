9a928663a462c0fa2cebfd3072a4e1559a50ec2f
Jeff Benton <jeff@frogrock.net>
2019-11-18 00:59:43 -0500

    began filterList class selection rework

--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -1,43 +1,31 @@
 import React, { Component } from 'react';
 
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 class FilterList extends Component {
 
-    selectClass = e => {
-        console.log(e.target.value)
+    state = {
+        class: 'Select Class'
+    };
+
+    handleClassSelection = e => {
+        this.setState({
+            class: e.target.title
+        })
     };
 
     render() {
         return (
-            <Button onClick={this.selectClass}>Select Class</Button>
+            <DropdownButton id="dropdown-class-button" title={this.state.class}>
+                {['Death Knight', 'Demon Hunter', 'Druid', 'Hunter', 'Mage', 'Monk', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior'].map(cls => (
+                    <div key={cls}>
+                        <Dropdown.Item onClick={this.handleClassSelection} title={cls}>{cls}</Dropdown.Item>
+                    </div>
+                ))}
+            </DropdownButton>
         )
     }
 }
 
 export default FilterList
-
-// <Form>
-// <Form.Label>Class</Form.Label>
-// {['Death Knight', 'Demon Hunter', 'Druid', 'Hunter', 'Mage', 'Monk', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior'].map(cls => (
-//     <div key={cls}>
-//         <Form.Check
-//             type={'radio'}
-//             id={cls}
-//             label={cls}
-//         />
-//     </div>
-// ))}
-//
-// <Form.Label>Source</Form.Label>
-// {['Raid', 'Dungeon', 'PvP'].map(src => (
-//     <div key={src}>
-//         <Form.Check
-//             type={'checkbox'}
-//             id={src}
-//             label={src}
-//         />
-//     </div>
-// ))}
-// </Form>
