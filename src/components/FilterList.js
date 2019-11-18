1b8786e341d247157738410f9a3786fa8275e0fb
Jeff Benton <jeff@frogrock.net>
2019-11-18 02:35:20 -0500

    filterlist state updates

--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 class FilterList extends Component {
 
     state = {
-        class: 'Select Class'
+        class: 'Select Class',
+        sources: {
+            raid: false,
+            dungeon: false,
+            pvp: false
+        }
     };
 
     handleClassSelection = e => {
@@ -15,15 +23,44 @@ class FilterList extends Component {
         })
     };
 
+    handleSourceSelection = e => {
+        const source = e.target.id.toLowerCase();
+        const sources = this.state.sources;
+        sources[source] = !this.state.sources[source];
+
+        this.setState({
+            sources
+        })
+    };
+
     render() {
+        console.log(this.state);
         return (
-            <DropdownButton id="dropdown-class-button" title={this.state.class}>
-                {['Death Knight', 'Demon Hunter', 'Druid', 'Hunter', 'Mage', 'Monk', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior'].map(cls => (
-                    <div key={cls}>
-                        <Dropdown.Item onClick={this.handleClassSelection} title={cls}>{cls}</Dropdown.Item>
-                    </div>
-                ))}
-            </DropdownButton>
+            <Container>
+                <Row>
+                    <DropdownButton id="dropdown-class-button" title={this.state.class}>
+                        {['Death Knight', 'Demon Hunter', 'Druid', 'Hunter', 'Mage', 'Monk', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior'].map(cls => (
+                            <div key={cls}>
+                                <Dropdown.Item onClick={this.handleClassSelection} title={cls}>{cls}</Dropdown.Item>
+                            </div>
+                        ))}
+                    </DropdownButton>
+
+                    <Form>
+                        {['Raid', 'Dungeon', 'PvP'].map(source => (
+                            <div key={source}>
+                                <Form.Check
+                                    onClick={this.handleSourceSelection}
+                                    custom
+                                    label={source}
+                                    type="checkbox"
+                                    id={source}
+                                />
+                            </div>
+                        ))}
+                    </Form>
+                </Row>
+            </Container>
         )
     }
 }
