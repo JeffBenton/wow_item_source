c3ddbb305e9dda1bfd9878fd5bed0408a9488090
Jeff Benton <jeff@frogrock.net>
2019-11-25 03:33:29 -0500

    converted itembutton to functional component

--- client/src/components/ItemButton.js
+++ client/src/components/ItemButton.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Button from 'react-bootstrap/Button'
 import { NavLink } from 'react-router-dom'
 
-class ItemButton extends Component {
-
-    render() {
-        const name = this.props.name.toLowerCase();
-        return (
-            <NavLink to={`/${name}`} activeStyle={{ disabled: true }} id={name}><Button>{this.props.name}</Button></NavLink>
-
-
-        )
-    }
-}
+const ItemButton = props => {
+    const name = props.name.toLowerCase();
+    return (
+        <NavLink to={`/${name}`} activeStyle={{ disabled: true }} id={name}><Button>{props.name}</Button></NavLink>
+    )
+};
 
 export default ItemButton
